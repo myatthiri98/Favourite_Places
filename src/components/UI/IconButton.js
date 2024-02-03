9b350420e2e00f79bf4c21ff84024bc16a19f119
Myat Thiri Khin <137380130+myatthiri98@users.noreply.github.com>
2024-02-03 22:59:28 +0630

    feat : Adding screens and set up navigation
    feat : Adding screens and set up navigation

---
+++ src/components/UI/IconButton.js
@@ -0,0 +1,29 @@
+import { Pressable, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Icon from 'react-native-vector-icons/Ionicons';
+
+
+const IconButton = ({icon,size,color,onPress}) => {
+  return (
+    <Pressable
+    style={({ pressed }) => [styles.button, pressed && styles.pressed]}
+    onPress={onPress}
+  >
+<Icon name={icon} size={size} color={color} />
+    </Pressable>
+  )
+}
+
+export default IconButton
+
+const styles = StyleSheet.create({
+  button: {
+    padding: 8,
+    margin: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  pressed: {
+    opacity: 0.7,
+  },
+})
---
+++ src/screens/AllPlaces.js
@@ -0,0 +1,12 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import PlaceItem from '../components/Places/PlaceItem';
+import PlacesList from '../components/Places/PlacesList';
+
+const AllPlaces = () => {
+  return <PlacesList />;
+};
+
+export default AllPlaces;
+
+const styles = StyleSheet.create({});
